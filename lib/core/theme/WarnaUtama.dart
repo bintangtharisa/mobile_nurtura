9f611a574fcd1470fb5c700fb8f2eb5da103d6e8
bintangtharisa <bintangtharisa@gmail.com>
2026-04-03 16:43:11 +0700

    bintang tambah warna utama

--- lib/core/theme/WarnaUtama.dart
+++ lib/core/theme/WarnaUtama.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 class WarnaUtama {
   static const background = Color(0xFFF8F5F2);
-  static const button1 = Color(0xFFA3B18B);
-  static const button2 = Color(0xFFDDBEA9);
+  static const button = Color(0xFFA3B18B);
+  static const primary = Color(0xFFDDBEA9);
+  static const secondary = Color(0xFFA3B18A);
+  static const form = Color(0xFFEDE0D4);
+  static const text1 = Color(0xFF3A3A3A);
+  static const text2 = Color(0xFFFFFFFF);
+
+  static const kotakLogin = Color(0xFFF6CBB5);
 }

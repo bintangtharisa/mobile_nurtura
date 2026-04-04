79c45dd4c842525ecd464c0946cef6300dea6408
bintangtharisa <bintangtharisa@gmail.com>
2026-04-04 22:42:39 +0700

    bintang nambahin judul tiap text field

--- lib/features/shared/views/register.dart
+++ lib/features/shared/views/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/gestures.dart';
 import '../../../../core/theme/warna_utama.dart';
 import '../widgets/text_field.dart';
 import '../widgets/button.dart';
@@ -28,6 +29,7 @@ class _RegisterPageState extends State<RegisterPage> {
             Navigator.pop(context);
           },
         ),
+
       centerTitle: true,
       title: Text(
         "Nurtura",
@@ -38,6 +40,7 @@ class _RegisterPageState extends State<RegisterPage> {
            ),
           ),
         ),
+        
       body: SafeArea(
         child: SingleChildScrollView(
         child: Padding(
@@ -76,7 +79,17 @@ class _RegisterPageState extends State<RegisterPage> {
 
               SizedBox(height: 20),
 
-              // Role Selector
+              Align(alignment: AlignmentGeometry.centerLeft,
+                child: Text(
+                  "Pilih Role Anda",
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: WarnaUtama.text1,
+                  ),
+                ),
+              ),
               PilihRole(
                 selectedRole: selectedRole,
                 onSelected: (role) {
@@ -89,10 +102,48 @@ class _RegisterPageState extends State<RegisterPage> {
               SizedBox(height: 20),
 
               // Form
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Nama Lengkap",
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: WarnaUtama.text1,
+                  ),
+                ),
+              ),
               CustomTextField(hint: "Nama Lengkap", icon: Icons.person),
               SizedBox(height: 12),
+
+                            Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Email",
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: WarnaUtama.text1,
+                  ),
+                ),
+              ),
               CustomTextField(hint: "Email", icon: Icons.email),
               SizedBox(height: 12),
+
+                            Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Kata Sandi",
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: WarnaUtama.text1,
+                  ),
+                ),
+              ),
               CustomTextField(hint: "Kata Sandi", obscure: true, icon: Icons.lock),
               SizedBox(height: 20),
 
@@ -103,14 +154,38 @@ class _RegisterPageState extends State<RegisterPage> {
 
               SizedBox(height: 20),
 
-              Text("Sudah punya akun? Masuk",
-                style: TextStyle(
-                  fontFamily: 'Manrope',
-                  color: WarnaUtama.text1,
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    color: WarnaUtama.text1.withOpacity(0.6),
+                  ),
+                  children: [
+                    TextSpan(text: "Sudah punya akun? "),
+                    TextSpan(
+                      text: "Masuk",
+                      style: TextStyle(
+                        color: WarnaUtama.kotakLogin,
+                        fontWeight: FontWeight.w600,
+                        decoration: TextDecoration.underline,
+                        decorationColor: WarnaUtama.primary,
+                        decorationThickness: 1.5,
+                      ),
+                      // recognizer: TapGestureRecognizer()
+                      //   ..onTap = () {
+                      //     Navigator.pushReplacement(
+                      //       context,
+                      //       MaterialPageRoute(
+                      //         builder: (context) => LoginPage(),
+                      //       ),
+                          // );
+                        // },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-
-            ],
+              ],
             ),
           ),
         ),

d9a45081bdafbc3cc5c1135a084fa5c0f70779cc
bintangtharisa <bintangtharisa@gmail.com>
2026-05-26 19:32:07 +0700

    menambahkan chatbot ayah

--- lib/features/ayah/views/beranda.dart
+++ lib/features/ayah/views/beranda.dart
@@ -7,6 +7,7 @@ import '../../../services/father_service.dart';
 import '../../shared/widgets/chatbot_card.dart';
 import '../../shared/views/lihat_tips_page.dart';
 import 'notifikasi_ayah_page.dart';
+import '../../shared/views/chatbot_page.dart';
 
 class BerandaAyahPage extends StatefulWidget {
   const BerandaAyahPage({super.key});
@@ -128,9 +129,25 @@ class _BerandaAyahPageState extends State<BerandaAyahPage> {
                 ChatbotCard(
                   namaBot: 'Nurtura AI',
                   pesanAwal: 'Halo Ayah! 👋 Ada yang bisa aku bantu hari ini?',
-                  onOpenChat: () {
-                    // TODO: navigasi ke halaman chatbot
-                  },
+                  onOpenChat: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const ChatbotPage(
+                        pesanAwal: 'Halo Ayah! 👋 Ada yang bisa aku bantu hari ini?',
+                        quickReplies: ['Saya merasa lelah', 'Tips menyusui', 'Jadwal imunisasi'],
+                      ),
+                    ),
+                  ),
+                  onKirimPesan: (teks) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ChatbotPage(
+                        pesanAwal: 'Halo Ayah! 👋 Ada yang bisa aku bantu hari ini?',
+                        quickReplies: ['Tips menangani mood swings', 'Cara mengatasi stres', 'Rekomendasi aktivitas bersama anak'],
+                        pesanPertama: teks,
+                      ),
+                    ),
+                  ),
                 ),
 
                   const SizedBox(height: 28),
